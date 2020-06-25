8e8ebcc0836b0ce84fb854bdeedd1b6455e54633
Arthur Guédon <arthur.guedon@student-cs.fr>
2020-06-25 19:19:11 +0200

    heroku log

--- src/server.js
+++ src/server.js
@@ -43,7 +43,7 @@ const io = socketio(server,{
     handlePreflightRequest: (req, res) => {
         const headers = {
             "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": process.env.FRONTEND_URL, 
+            "Access-Control-Allow-Origin": req.headers.origin,//process.env.FRONTEND_URL, 
             "Access-Control-Allow-Credentials": true
         };
         res.writeHead(200, headers);
@@ -58,9 +58,9 @@ const io = socketio(server,{
 io.use(socketioAuth)
 io.origins((origin, callback) => {
     
-    if (origin !== process.env.FRONTEND_URL) {
-        return callback('origin not allowed', false);
-    }
+    // if (origin !== process.env.FRONTEND_URL) {
+    //     return callback('origin not allowed', false);
+    // }
     callback(null, true);
   })
 const liveMessaging = generateLiveMessage(io)
@@ -81,7 +81,7 @@ app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(express.json())
 //Middleware permettant de gérer les CORS, à changer pour le passage en prod
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL)
+    res.header("Access-Control-Allow-Origin", "*")//process.env.FRONTEND_URL)
     res.header("Access-Control-Allow-Headers", "*")
     res.header("Access-Control-Allow-Methods","PUT,GET,POST,DELETE,PATCH")
     next();
